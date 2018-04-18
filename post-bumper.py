fd2777fb4d73e9b5db604e111b0676f90b1adb28
cqmpact <35305953+cqmpact@users.noreply.github.com>
2018-04-18 13:26:51 +0200

    Update post-bumper.py

--- post-bumper.py
+++ post-bumper.py
@@ -16,49 +16,9 @@ reddit = praw.Reddit(client_id='id',
                      username='username',
                      password='pw')
 
-subreddits = ['DiscordLinks', 'discordserver', 'DiscordAdvertising', 'DiscordAppServers', 'MakeDiscordFriends']
-# subreddits = ['DiscordLinks', 'discordserver', 'DiscordAdvertising']
-pos = 0
-errors = 0
+subreddit = reddit.subreddit('test')
+for submission in subreddit.hot(limit=5):
 
-title = "blu_ | VOICE | New community looking for active members, come check us out! Our primary game is PUBG!"
-url = "https://discord.me/blu"
-
-def post():
-    global subreddits
-    global pos
-    global errors
-
-    try:
-        subreddit = reddit.subreddit(subreddits[pos])
-        subreddit.submit(title, url=url)
-        print("Posted to " + subreddits[pos])
-
-        pos = pos + 1
-
-        if (pos <= len(subreddits) -1):
-            post()
-        else:
-           print("Done")
-    except praw.exceptions.APIException as e:
-            if (e.error_type == "RATELIMIT"):
-                delay = re.search("(\d+) minutes?", e.message)
-                print (e.message)
-                if delay:
-                    delay_seconds = float(int(delay.group(1)) * 60)
-                    time.sleep(delay_seconds)
-                    post()
-                else:
-                        delay = re.search("(\d+)", e.message)
-                        delay_seconds = float(delay.group(1))
-                        time.sleep(delay_seconds)
-                        post()
-    except:
-        errors = errors + 1
-        if (errors > 5):
-          print("Crashed")
-          exit(1)
-
-    post()
-
-post()
+        if re.search("TITLE KEYWORD", submission.title, re.IGNORECASE):
+            submission.reply("The robots are taking over!")
+            print("Bot replying to : ", submission.title)
